3dafa8526064b734f2a8fef2a0aaca2fbd4daff2
KhaiYuan22 <khaiyuan.cheah@beans.com.my>
2025-06-25 16:18:19 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+	@Bean
+	CommandLineRunner commandLineRuner(StudentRepository repository) {
+		return args -> {
+			Student stud1 = new Student("stud1", "s1@gmail.com", LocalDate.of(2002, 10, 10));
+			Student stud2 = new Student("stud2", "s1@gmail.com", LocalDate.of(2012, 12, 28));
+			
+			repository.saveAll(
+					List.of(stud1,stud2)
+					);
+		};
+	}
+}
